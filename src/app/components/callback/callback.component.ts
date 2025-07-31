f4be39d8a843fb768b64a00891eea918cdd120b8
tommybrightly <tommybrightly@gmail.com>
2025-07-30 20:48:49 -0500

    added callback component

---
+++ src/app/components/callback/callback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callback',
+  standalone: true,
+  imports: [],
+  templateUrl: './callback.component.html',
+  styleUrl: './callback.component.css'
+})
+export class CallbackComponent {
+
+}
