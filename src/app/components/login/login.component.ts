5be2dbdfda98eedb328577eb0e7b798c3dba4bc3
tommybrightly <tommybrightly@gmail.com>
2025-07-30 20:45:54 -0500

    connected auth service to login button

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,6 @@ import { SpotifyAuthService } from '../../services/spotify-auth.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,7 +10,7 @@ export class LoginComponent {
   constructor(private spotifyAuth: SpotifyAuthService) {}
 
   login(): void {
-    window.location.href = this.spotifyAuth.getAuthUrl();
+    this.spotifyAuth.loginWithSpotify()
   }
 
 }
