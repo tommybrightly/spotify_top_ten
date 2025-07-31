6d95f6c1f73a4394b10a578ef771d30dc6a31550
tommybrightly <tommybrightly@gmail.com>
2025-07-31 10:56:01 -0500

    fixed httpClient injection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+  
 };
