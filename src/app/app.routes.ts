0e7834e96b4792f3f524d1484165703afbf6c435
tommybrightly <tommybrightly@gmail.com>
2025-07-30 20:59:27 -0500

    added route for callback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CallbackComponent } from './components/callback/callback.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
+    {path: 'callback', component: CallbackComponent},
     {path: '', redirectTo: 'login', pathMatch:'full'},
     {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
